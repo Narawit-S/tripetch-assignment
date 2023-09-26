3d47f591e47da30566d03ed883a5970580b407e2
Narawit Sukwattanajaroon <narawichsukwattanajaroon@gmail.com>
2023-09-26 23:39:40 +0700

    feat: players mobile

--- src/features/sports/components/Players.tsx
+++ src/features/sports/components/Players.tsx
@@ -1,14 +1,33 @@
 import Image from 'next/image'
-import { AspectRatio, Box, Stack, Text } from '@chakra-ui/react'
+import { Autoplay, Pagination, Navigation } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import {
+  AspectRatio,
+  Box,
+  Flex,
+  Stack,
+  Text,
+  useBreakpointValue,
+} from '@chakra-ui/react'
 
 import { ContentNumber } from './ContentNumber'
+
 import {
   firstContentPadding,
   secondContentPadding,
   thirdContentPadding,
 } from '../styles'
+import 'swiper/css'
+import 'swiper/css/pagination'
 
 export const Players = () => {
+  const isMobile = useBreakpointValue({
+    base: true,
+    sm: false,
+  })
+
+  if (isMobile) return <PlayersMobile />
+
   return (
     <Box position="relative" overflow="hidden">
       <AspectRatio
@@ -76,5 +95,68 @@ export const Players = () => {
         </Stack>
       </Box>
     </Box>
+  )
+}
+
+const PlayersMobile = () => {
+  return (
+    <>
+      <Flex sx={{ flexDirection: 'column', px: '5.625vw' }}>
+        <Text
+          sx={{
+            color: 'gray.200',
+            fontSize: '15.625vw',
+            textTransform: 'uppercase',
+          }}
+        >
+          players
+        </Text>
+        <AspectRatio
+          ratio={302 / 250}
+          sx={{ alignSelf: 'center', w: '94.375vw', mt: '6.875vw' }}
+        >
+          <Image src="/sports/basketball.svg" alt="basketball" fill />
+        </AspectRatio>
+      </Flex>
+      <Box layerStyle="swiperContainer">
+        <Swiper
+          loop={true}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+          spaceBetween={30}
+          pagination={{ clickable: true }}
+          modules={[Autoplay, Pagination, Navigation]}
+        >
+          <SwiperSlide>
+            <Stack spacing={5}>
+              <ContentNumber count="01" title="connection" />
+              <Text fontSize="4.688vw">
+                Connect with talented athlete directly, you can watch their
+                skills through video showreels directly from Surface 1.
+              </Text>
+            </Stack>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Stack spacing={5}>
+              <ContentNumber count="02" title="collaboration" />
+              <Text fontSize="4.688vw">
+                Work with recruiter to increase your chances of findingtalented
+                athlete.
+              </Text>
+            </Stack>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Stack spacing={5}>
+              <ContentNumber count="03" title="growth" />
+              <Text fontSize="4.688vw">
+                Save your time, recruit proper athlets for your team.
+              </Text>
+            </Stack>
+          </SwiperSlide>
+        </Swiper>
+      </Box>
+    </>
   )
 }
