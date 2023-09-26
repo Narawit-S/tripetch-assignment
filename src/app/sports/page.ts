706739ab602854e2cea8c48d2f20e6e0c82df870
Narawit Sukwattanajaroon <narawichsukwattanajaroon@gmail.com>
2023-09-26 23:59:01 +0700

    feat: meta title for sport page

--- src/app/sports/page.ts
+++ src/app/sports/page.ts
@@ -1,1 +1,7 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Sport Page - Tri Petch Assignment',
+}
+
 export { default } from '@/features/sports/pages/SportsPage'
