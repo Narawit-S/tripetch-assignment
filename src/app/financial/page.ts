781edda8ed2791a041e4c491cf406fc34e7575c6
Narawit Sukwattanajaroon <narawichsukwattanajaroon@gmail.com>
2023-09-27 23:58:48 +0700

    feat: financial meta title

--- src/app/financial/page.ts
+++ src/app/financial/page.ts
@@ -1,1 +1,7 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Financial Page - Tri Petch Assignment',
+}
+
 export { default } from '@/features/financial/pages/FinancialPage'
